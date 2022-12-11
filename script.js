99471351c0faa457b6e870a7ab2c132915082b51
RSmith484 <117265457+RSmith484@users.noreply.github.com>
2022-12-11 12:15:57 +1100

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,8 @@ let selectedRating = '5'
 
 // Find out which rating was selected
 ratingsContainer.addEventListener('click', (e) => {
-    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.nextElementSibling.innerHTML
-    } else if(
-        e.target.parentNode.classList.contains('rating')
-    ) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.innerHTML
+    if(e.target.checked) {
+        selectedRating = e.target.value;
     }
 })
 
@@ -24,16 +16,16 @@ submitBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
         <img src="./images/illustration-thank-you.svg">
         <p id="thank-you">You selected ${selectedRating} out of 5</p>
-        <h1>Thank You!</h1>
+        <h1 id="text-align">Thank You!</h1>
         <br>
-        <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+        <p id="text-align">We appreciate you taking the time to give a rating. If you ever need more support, 
         don’t hesitate to get in touch!</p>
     `
 })
 
-// Utility function to remove the 'active' class from ratings
-function removeActive() {
+// Utility function to remove the 'checked' property from ratings
+function removeChecked() {
     for(let i = 0; i < ratings.length; i++) {
-        ratings[i].classList.remove('active')
+        ratings[i].remove('checked')
     }
 }
